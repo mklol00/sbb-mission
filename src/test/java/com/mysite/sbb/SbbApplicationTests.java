5e458eb9ecdfd2bcdafe716c0d395b8a2d17bc50
mklol00 <dlwodn97@naver.com>
2023-11-27 15:42:43 +0900

    2-05 findById

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -3,24 +3,31 @@ package com.mysite.sbb;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.List;
-
+import java.util.Optional;
 import java.time.LocalDateTime;
+import java.util.function.Function;
 
 @SpringBootTest
 class SbbApplicationTests {
 
     @Autowired
     private QuestionRepository questionRepository;
-
     @Test
     void testJpa() {
-        List<Question> all = this.questionRepository.findAll();
-        assertEquals(2,all.size());
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if(oq.isPresent()){
+            Question q =oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
 
-        Question q = all.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
     }
 
 }
